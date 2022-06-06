d69b997c983be7c85a6b7d80125483e5b1c23fa1
linuxlivre <82552841+tiaoedi@users.noreply.github.com>
2022-06-06 08:48:42 -0300

    Add files via upload

---
+++ script/400-remove-software.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# when on CARLI - remove conflicting files 
+
+if [ -f /usr/local/bin/get-nemesis-on-carli ]; then
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Removing software from Carli"
+  echo "################################################################"
+  tput sgr0
+  echo
+  if [ -f /etc/skel/.bashrc ]; then
+    sudo rm /etc/skel/.bashrc
+  fi  
+  sudo pacman -R --noconfirm carli-xfce-config
+  sudo pacman -R --noconfirm grml-zsh-config
+  sudo pacman -R --noconfirm systemd-resolvconf
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Software removed"
+  echo "################################################################"
+  tput sgr0
+  echo
+
+fi
+
+# when on ARISER - remove conflicting files 
+
+if [ -f /usr/local/bin/get-nemesis-on-ariser ]; then
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Removing software from ARISER"
+  echo "################################################################"
+  tput sgr0
+  echo
+  if [ -f /etc/skel/.bashrc ]; then
+    sudo rm /etc/skel/.bashrc
+  fi
+  #sudo rm /etc/skel/.Xresources
+  #sudo pacman -R --noconfirm grml-zsh-config
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Software removed"
+  echo "################################################################"
+  tput sgr0
+  echo 
+fi
+
+# when on ARCOLINUX - remove conflicting files
+
+if [ -f /usr/local/bin/get-nemesis-on-arcolinux ]; then
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "#######Software to remove from an ArcoLinux installation"
+  echo "################################################################"
+  tput sgr0
+  echo
+
+  sudo systemctl disable tlp.service
+  sudo pacman -Rs tlp --noconfirm
+  sudo pacman -Rs broadcom-wl-dkms --noconfirm
+  sudo pacman -Rs xf86-video-amdgpu --noconfirm
+  sudo pacman -Rs xf86-video-fbdev --noconfirm
+  sudo pacman -Rs xf86-video-openchrome --noconfirm
+  if pacman -Qi xf86-video-vmware &> /dev/null; then
+    sudo pacman -Rs xf86-video-vmware --noconfirm
+  fi
+  sudo pacman -Rs xf86-video-ati --noconfirm
+  sudo pacman -Rs xf86-video-nouveau --noconfirm
+  sudo pacman -Rs xf86-video-vesa --noconfirm
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Software removed"
+  echo "################################################################"
+  tput sgr0
+  echo
+
+fi
+
+# when on EOS - remove conflicting files
+
+if grep -q "EndeavourOS" /etc/os-release; then
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "############### Removing software for EOS"
+  echo "################################################################"
+  tput sgr0
+
+  if [ -f /etc/skel/.bashrc ]; then
+    sudo rm /etc/skel/.bashrc
+  fi
+
+  sudo systemctl disable firewalld
+  sudo pacman -R --noconfirm firewalld
+
+
+  sudo pacman -R --noconfirm arc-gtk-theme-eos
+  sudo pacman -R --noconfirm endeavouros-skel-default endeavouros-skel-xfce4
+  sudo pacman -R --noconfirm modemmanager
+  sudo pacman -R --noconfirm yay
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Software removed"
+  echo "################################################################"
+  tput sgr0
+  echo
+
+fi
+
+# when on ALCI - remove conflicting files
+
+if [ -f /usr/local/bin/get-nemesis-on-alci ]; then
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "############### Removing software for ALCI"
+  echo "################################################################"
+  tput sgr0
+
+  if [ -f /etc/skel/.bashrc ]; then
+    sudo rm /etc/skel/.bashrc
+  fi
+  sudo rm /etc/skel/.Xresources
+  sudo pacman -R --noconfirm amd-ucode
+  sudo pacman -R --noconfirm b43-fwcutter
+  sudo pacman -R --noconfirm broadcom-wl
+  sudo pacman -R --noconfirm broadcom-wl-dkms  
+  sudo pacman -Rs --noconfirm cloud-init
+  sudo pacman -R --noconfirm darkhttpd
+  sudo pacman -R --noconfirm dhcpcd
+  sudo pacman -R --noconfirm ell  
+  sudo pacman -R --noconfirm grml-zsh-config
+  sudo pacman -R --noconfirm iwd
+  sudo pacman -R --noconfirm kitty-terminfo
+  sudo pacman -R --noconfirm lftp
+  sudo pacman -R --noconfirm livecd-sounds
+  sudo pacman -R --noconfirm lua53
+  sudo pacman -R --noconfirm luit
+  sudo pacman -R --noconfirm lynx
+  sudo pacman -R --noconfirm mousepad
+  sudo pacman -R --noconfirm nmap
+  sudo pacman -R --noconfirm parole
+  sudo pacman -R --noconfirm systemd-resolvconf
+  sudo pacman -R --noconfirm xbitmaps
+  sudo pacman -R --noconfirm xfburn
+  sudo pacman -R --noconfirm xfce4-artwork
+  sudo pacman -R --noconfirm xterm
+  sudo pacman -Rs --noconfirm brltty
+  sudo pacman -Rs --noconfirm espeak-ng
+  sudo pacman -Rs --noconfirm espeakup
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Software removed"
+  echo "################################################################"
+  tput sgr0
+  echo
+
+fi
+
+
+# when on Garuda - remove conflicting files
+
+if grep -q "Garuda" /etc/os-release; then
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "############### Removing software for EOS"
+  echo "################################################################"
+  tput sgr0
+
+  if [ -f /etc/skel/.bashrc ]; then
+    sudo rm /etc/skel/.bashrc
+  fi
+
+  sudo pacman -R --noconfirm blueman
+  sudo pacman -R --noconfirm garuda-xfce-settings
+  sudo pacman -R --noconfirm garuda-common-settings
+  sudo pacman -R --noconfirm garuda-bash-config
+  sudo pacman -R --noconfirm redshift
+
+  echo
+  tput setaf 2
+  echo "################################################################"
+  echo "################### Software removed"
+  echo "################################################################"
+  tput sgr0
+  echo
+
+fi
---
+++ script/README.md
@@ -0,0 +1,75 @@
+# ArcoLinux Nemesis
+
+What to do after the installation of the latest version of ArcoLinux?
+
+Personally I will install these applications with the use of the scripts.
+These are applications not everyone would want on an iso.
+
+Learn, have fun and enjoy.
+
+
+# Websites
+
+Information : https://arcolinux.info
+
+Tutorials about ArcoLinux iso (Xfce, Openbox and i3) : https://arcolinux.com
+
+Tutorials about ArcoLinuxD iso (any desktop) : https://arcolinuxd.com
+
+Tutorials about ArcoLinuxB iso (any desktop) : https://arcolinuxb.com
+
+Tutorials about Carli project (any desktop) : https://arcolinuxiso.com
+
+Forum : https://arcolinuxforum.com
+
+
+# Social Media
+
+Facebook : https://www.facebook.com/arcolinuxd/
+
+Facebook : https://www.facebook.com/groups/arcolinux/
+
+Twitter  : https://twitter.com/arcolinux
+
+Instagram: https://www.instagram.com/arcolinux/
+
+Linked in: https://www.linkedin.com/in/arcolinux
+
+Youtube  : https://www.youtube.com/erikdubois
+
+
+# Arcolinux keys for anything Arch Linux based
+
+
+Get keys and mirrors from ArcoLinux
+
+The ArcoLinux repos are added to your /etc/pacman.conf
+
+
+`wget bit.ly/get-arcolinux-keys && chmod +x ./get-arcolinux-keys && sudo ./get-arcolinux-keys`
+
+
+Get keys and mirrors from ArcoLinux and install <b>archlinux-tweak-tool-git</b>
+
+The ArcoLinux repos are added to your /etc/pacman.conf
+
+
+`wget bit.ly/get-arcolinux-keys && chmod +x ./get-arcolinux-keys  && sudo ./get-arcolinux-keys && sudo pacman -Syu archlinux-tweak-tool-git`
+
+
+# ArchLinux Tweak Tool info
+
+ArchLinux Tweak Tool will work on these distro's
+
+- ArcoLinux     - https://arcolinux.info
+- Arch Linux    - https://archlinux.org
+- Alci          - https://alci.online
+- Carli         - https://arcolinuxiso.com/
+- Ariser        - https://ariser.eu/
+- EndeavourOS   - https://endeavouros.com/
+- Garuda        - https://garudalinux.org/
+
+
+ArchLinux Tweak Tool will NOT work on these distro's
+
+- Manjaro
